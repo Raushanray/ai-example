f65d8cd95277461f58e995eb6915d48c8b8ec3ad
Raushanray <raushankuray1998@gmail.com>
2024-05-03 11:13:33 +0530

    create an API as Dynamic.

--- src/main/java/com/ai/example/AIController.java
+++ src/main/java/com/ai/example/AIController.java
@@ -3,6 +3,7 @@ package com.ai.example;
 import org.springframework.ai.ollama.OllamaChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,16 +12,24 @@ public class AIController {
 
     private OllamaChatClient client;
 
-    private static final String PROMPT = "what is C language";
+//    private static final String PROMPT = "what is C language";
 
     public AIController(OllamaChatClient client) {
         this.client = client;
     }
 
 
-    @GetMapping("/prompt")
+    /*@GetMapping("/prompt")
     private String promptResponse(){
         String response = client.call(PROMPT);
+        return response;
+    }*/
+
+    @GetMapping("/prompt")
+    private String promptResponse(
+            @RequestParam("prompt") String prompt
+    ){
+        String response = client.call(prompt);
         return response;
     }
 }
